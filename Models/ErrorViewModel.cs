6561b8625940b3019d58da47e59c928aaeb383b9
MSOliveiraa <marco.oliveira.1564235@sga.pucminas.br>
2025-10-14 23:25:17 +0100

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SaberMais.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
